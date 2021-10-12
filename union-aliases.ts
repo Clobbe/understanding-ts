58cb1ed3130c86a3cccb53d0a249cd8daabd6ced
Clobbe Norman <clobbe.norman@gmail.com>
2021-10-12 16:42:47 +0200

    testing type Function

---
+++ union-aliases.ts
@@ -0,0 +1,25 @@
+type Combine = string | number;
+
+function combine(
+  input1: Combine,
+  input2: Combine,
+  resultOutput: "as-number" | "as-string"
+) {
+  let result;
+  if (
+    typeof input1 === "number" &&
+    typeof input2 === "number" &&
+    resultOutput === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combinedNames = combine("Clobbe", "doing TS", "as-string");
+const combinedAge = combine(29, 30, "as-number");
+
+console.log(combinedNames);
+console.log(combinedAge);
