84b6171782c9a82762111ef48d413701b8d40e5e
Clobbe Norman <clobbe.norman@gmail.com>
2021-10-12 16:52:31 +0200

    adding the concept of using callback function in function

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log("result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, callback: (num: number) => void) {
+  const result = n1 + n2;
+  callback(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
@@ -13,3 +18,7 @@ let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
