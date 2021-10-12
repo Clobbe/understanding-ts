81de46f49db4486407c62c3456704e783fa0520c
Clobbe Norman <clobbe.norman@gmail.com>
2021-10-12 17:05:46 +0200

    testing and learning the way of the never-type

--- app.ts
+++ app.ts
@@ -1,24 +1,16 @@
-function add(num1: number, num2: number): number {
-  return num1 + num2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log("result: " + num);
+function generateError(message: string, code: number): never {
+  throw { message: message, code: code };
 }
+// using the "never"-type since this breaks the code anyway.
 
-function addAndHandle(n1: number, n2: number, callback: (num: number) => void) {
-  const result = n1 + n2;
-  callback(result);
-}
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-// with this combineValues take a,b: number and expect a number as return type
-
-combineValues = add;
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+generateError("Something failed", 500);
