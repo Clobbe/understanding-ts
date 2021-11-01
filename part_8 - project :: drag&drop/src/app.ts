10bf1d080f25c3de20ab37ff3b06c8ba7a33cd52
Clobbe Norman <clobbe.norman@gmail.com>
2021-11-01 10:02:57 +0100

    adding some more private methods to render and listen for submission

--- part_8 - project :: drag&drop/src/app.ts
+++ part_8 - project :: drag&drop/src/app.ts
@@ -3,6 +3,11 @@ class ProjectInput {
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
 
+  //these fields are the elements inside the form. Elements which we'll want to access.
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
     this.templateElement = document.getElementById(
       'project-input'
@@ -15,8 +20,31 @@ class ProjectInput {
     ); //this will import the content of whatever the content is of the template.
     this.element = importedContent.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    )! as HTMLInputElement;
+    this.configure();
     this.attach();
   }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    //since the function is only suppose to be called from within the constructor, this is defined as a 'private' function
+    this.element.addEventListener('submit', this.submitHandler);
+    //this function 'listen' for the form-submission and then call the submitHandler-function
+  }
+
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element); //when calling this method, this will insert the imported template-HTML and insert this after the "opening-tag" of our div "app"
   }
