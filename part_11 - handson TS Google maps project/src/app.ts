0606f174779b025bb649dd7af037d0ee6cd2f5d2
Clobbe Norman <clobbe.norman@gmail.com>
2021-11-05 08:58:18 +0100

    ADD :: implemented first step to request GMaps-api

--- part_11 - handson TS Google maps project/src/app.ts
+++ part_11 - handson TS Google maps project/src/app.ts
@@ -1,11 +1,27 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement; // exclamation mark (!) tells TS that this won't be null.
+
+const gMapsApiKey = 'AIzaSyDEaoBTSphBIfvGf1jvr2s4asU8X85KmyI';
 
 const searchAddressHandler = (event: Event) => {
   event.preventDefault(); //this prevents that a HTTP-request is sent and so that the page it self don't reload.
   const enteredAddress = addressInput.value;
 
   // here code for calling GMaps-API go
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${gMapsApiKey}`
+    )
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 form.addEventListener('submit', searchAddressHandler);
