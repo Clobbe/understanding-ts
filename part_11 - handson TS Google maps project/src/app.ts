4e339f347b1348baa7e8c514a7687069800f80da
Clobbe Norman <clobbe.norman@gmail.com>
2021-11-05 08:37:04 +0100

    added missing files

--- part_11 - handson TS Google maps project/src/app.ts
+++ part_11 - handson TS Google maps project/src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement; // exclamation mark (!) tells TS that this won't be null.
+
+const searchAddressHandler = (event: Event) => {
+  event.preventDefault(); //this prevents that a HTTP-request is sent and so that the page it self don't reload.
+  const enteredAddress = addressInput.value;
+
+  // here code for calling GMaps-API go
+};
+
+form.addEventListener('submit', searchAddressHandler);
