5f06bf464cf6397fb3cfd7fd24f8af33d18e4eae
Clobbe Norman <clobbe.norman@gmail.com>
2021-11-05 09:05:09 +0100

    ADD :: type for the response from the request to GMaps-API.

--- part_11 - handson TS Google maps project/src/app.ts
+++ part_11 - handson TS Google maps project/src/app.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+type TGoogleGeocode = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+};
+/* this definition of our custom type ensure that we're telling TS what to expect in the response of the request to Gmaps-API */
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement; // exclamation mark (!) tells TS that this won't be null.
 
@@ -11,13 +16,14 @@ const searchAddressHandler = (event: Event) => {
 
   // here code for calling GMaps-API go
   axios
-    .get(
+    .get<TGoogleGeocode>(
+      /* by typing here, we'll also enable autocompletion/suggestions on the response =D  */
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${gMapsApiKey}`
     )
     .then((response) => {
-      console.log(response);
+      const coordinates = response.data.results[0].geometry.location;
     })
     .catch((err) => {
       console.log(err);
